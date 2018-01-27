8166b9dc4bcff1cd830f5b3aabf0c96ea82ed055
Sylvain YVON <sylvain.yvon@gmail.com>
2018-01-27 08:00:35 +0100

    eslinted

--- js/components.js
+++ js/components.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent("board", {
     schema: {
         chessBoardColumns: {type: "array", default: ["a", "b", "c", "d", "e", "f", "g", "h"]},
-        chessBoardRows: {type: "array", default:[8, 7, 6, 5, 4, 3, 2, 1]}
+        chessBoardRows: {type: "array", default: [8, 7, 6, 5, 4, 3, 2, 1]}
     },
     init: function () {
     }
