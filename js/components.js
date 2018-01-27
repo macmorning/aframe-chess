8a24378979b148e54f861faa75222e83376576ec
macmorning <sylvain.yvon@gmail.com>
2018-01-27 13:56:11 +0100

    moved board logic to component, added comments

--- js/components.js
+++ js/components.js
@@ -4,6 +4,31 @@ AFRAME.registerComponent("board", {
         chessBoardRows: {type: "array", default: [8, 7, 6, 5, 4, 3, 2, 1]}
     },
     init: function () {
+        while (this.el.lastChild) {
+            this.el.removeChild(this.el.lastChild);
+        }
+        this._drawBoard();
+    },
+    _drawBoard: function () {
+        let self = this;
+        self.data.chessBoardColumns.forEach(function (col, colIndex) {
+            self.data.chessBoardRows.forEach(function (row, rowIndex) {
+                // color of the new tile is easily determined; if row and column index are both even or odd, then it's white; else it's black
+                let color = (rowIndex % 2 === colIndex % 2 ? "white" : "black");
+                // create the new element in the dom
+                let el = document.createElement("a-entity");
+                // set the id of the new element : "a1", "c7"
+                el.id = col + row;
+                // use the mixins : [white|black] tile
+                el.setAttribute("mixin", color + " tile");
+                // declare that the new element has the tile component
+                el.setAttribute("tile", true);
+                // position is conveniently calculated based on the column and row index
+                el.setAttribute("position", (colIndex + 1) + " " + (rowIndex + 1) + " 0");
+                // add the element to the board element
+                self.el.appendChild(el);
+            });
+        });
     }
 });
 
