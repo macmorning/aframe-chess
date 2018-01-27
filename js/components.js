c08f23742849b99f6a0fa67188d2e1e47d174672
Sylvain YVON <sylvain.yvon@gmail.com>
2018-01-27 07:03:32 +0100

    Moving from codepen

---
+++ js/components.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('board', {
+    schema: {
+        chessBoardColumns: {type: "array", default: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']},
+        chessBoardRows: {type: "array", default:[8, 7, 6, 5, 4, 3, 2, 1]}
+    }
+    init: function () {
+    }
+});
+
+  
+
+AFRAME.registerComponent('tile', {
+    init: function () {
+    }
+});
---
+++ js/index.js
@@ -0,0 +1,39 @@
+const $ = (query) => document.querySelector(query);
+
+const board = {
+    chessBoardColumns: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+    chessBoardRows: [8, 7, 6, 5, 4, 3, 2, 1],
+    /*   
+     *   Initialize the board: remove all tiles and pieces
+     *
+     */
+     initChessBoard: function() {
+         let boardContainer = $("#boardContainer");
+         while (boardContainer.lastChild) {
+             boardContainer.removeChild(boardContainer.lastChild);
+         }
+         this._drawBoard(); 
+     },
+   
+     _drawBoard:function() {
+          let boardContainer = $("#boardContainer");
+          let self = this;
+          self.chessBoardColumns.forEach(function(col,colIndex) {  
+              self.chessBoardRows.forEach(function(row,rowIndex) {
+                  let color = (rowIndex%2 == colIndex%2 ? "white" : "black"); // clever, ain't it
+                  let el = document.createElement('a-entity');
+                  el.id = col + row;
+                  el.setAttribute("mixin",color + " tile");
+                  el.setAttribute("tile",true);
+                  el.setAttribute("position",(colIndex+1) + " " + (rowIndex+1) + " 0");
+                  boardContainer.appendChild(el);
+              });
+          });
+     }    
+ }
+ 
+ window.onload = function() {
+     board.initChessBoard();
+ 
+ }
+ 
