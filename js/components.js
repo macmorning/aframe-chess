e5de3fdd9785d01ef856c6ec3eaa5892086baefd
Sylvain YVON <sylvain.yvon@gmail.com>
2018-01-27 07:52:21 +0100

    eslinted

--- js/components.js
+++ js/components.js
@@ -1,15 +1,13 @@
-AFRAME.registerComponent('board', {
+AFRAME.registerComponent("board", {
     schema: {
-        chessBoardColumns: {type: "array", default: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']},
+        chessBoardColumns: {type: "array", default: ["a", "b", "c", "d", "e", "f", "g", "h"]},
         chessBoardRows: {type: "array", default:[8, 7, 6, 5, 4, 3, 2, 1]}
     },
     init: function () {
     }
 });
 
-  
-
-AFRAME.registerComponent('tile', {
+AFRAME.registerComponent("tile", {
     init: function () {
     }
 });
